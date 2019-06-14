0590f6d9ff0299a751d63e46c8fe1e8e0df5f70c
Mohamed Saeed <msaeed027@gmail.com>
2019-06-14 02:41:36 +0200

    Updates

---
+++ precache-manifest.163d07968cab13f635bd7966fa08597f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e6005df2bca0ed19aa934645886c62",
+    "url": "/todo-react-redux/index.html"
+  },
+  {
+    "revision": "7cf8ca6a0b08ebc32cdd",
+    "url": "/todo-react-redux/static/js/2.24581bd7.chunk.js"
+  },
+  {
+    "revision": "978c59ab93af2d2f50bc",
+    "url": "/todo-react-redux/static/js/main.37fe59cf.chunk.js"
+  },
+  {
+    "revision": "c2979c65fca7cd9446fe",
+    "url": "/todo-react-redux/static/js/runtime~main.9c2e1894.js"
+  }
+]);
